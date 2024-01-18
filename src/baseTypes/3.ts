eaafc4d068217a3eebe0476d794b4683481240ad
VadymPopov <boycristal10@gmail.com>
2024-01-18 15:23:22 -0500

    fixed

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,10 +1,9 @@
 let some:unknown;
 some = 'Text';
 let str: string;
-str = some as string;
 
-// if(typeof some === 'string'){
-//     str = some;
-// }
+if(typeof some === 'string'){
+    str = some;
+}
 
 export {};
